91c0ba1fb3d1fde2096d31873a0ba0a17c9858d7
Mythili Manasa Karra <mmkarra@unix2.lt.ucsc.edu>
2019-12-15 15:32:00 -0800

    Added pa1

---
+++ pa1/List.java
@@ -0,0 +1,327 @@
+//Mythili Karra
+//mmkarra
+//CS101
+//04/15/2019
+public class List {
+	private class Node {
+		int value;
+		Node next;
+		Node previous;
+
+		Node(int data) {
+			value = data;
+			next = null;
+			previous = null;
+		}
+		public String toString() {
+			String s = String.valueOf(value);
+			return s;
+
+		} 
+
+	}
+	private Node front;
+	private Node back;
+	private Node cursor;
+	private int length;
+	private int indexcursor;
+	public List() {
+		length = 0;
+		indexcursor = -1;
+		front = null;
+		back = null;
+		cursor = null;		
+	}
+	int length() {
+		return length;
+	}
+	int index() {
+		return indexcursor;
+	}
+	int front() {
+		if(length() > 0) //can u do just length?
+			return front.value;
+		else {
+			throw new RuntimeException("front() called on an empty list");           
+
+		}
+	}
+	int back() {
+		if(length() > 0) 
+			return back.value;
+		else {
+			throw new RuntimeException("back() called on an empty list");
+		}
+	}
+	int get() {
+		if(length>0 && indexcursor >=0) {
+			return cursor.value;
+		}
+		else {
+			throw new RuntimeException("get() called when there is no cursor element");
+		}
+	}
+	boolean equals(List L) {
+		boolean x = false;
+		if(length != L.length) {
+			return false;
+		}
+		else if(length == 0) {
+			return true;
+		}
+		else {
+			Node a = front;
+			Node b = L.front;
+			while(a != null) {
+				if(a.value != b.value) {
+					return false;
+				}
+				else {
+					a = a.next;
+					b = b.next;
+				}
+			}
+		}
+		return true;
+	}
+
+	void clear() {
+		front = null;
+		back = null;
+		cursor = null;
+		length = 0;
+		indexcursor = -1;
+	}
+	void moveFront() {
+		if(length() > 0) {
+			cursor = front;
+			indexcursor = 0;
+		}
+	}
+	void moveBack() {
+		if(length() > 0) {
+			cursor = back;
+			indexcursor = length-1;
+		}
+	}
+	void moveNext() {
+		if(cursor != null && cursor != back) {
+			Node a = cursor;
+			Node b = cursor.next.next;
+			cursor = cursor.next;
+			cursor.previous = a;
+			cursor.next = b;
+			indexcursor++;
+		}
+		else if(cursor != null && indexcursor == length - 1) {
+			cursor = null;
+			indexcursor = -1;
+		}
+	}
+	void movePrev() {
+		if(cursor != null) {
+			if(indexcursor != 0 && indexcursor != 1) {
+				Node a = cursor.previous.previous;
+				Node b = cursor;
+				cursor = cursor.previous;
+				cursor.previous = a;
+				cursor.next = b;
+				indexcursor--;
+			}
+			else if(indexcursor == 0) {
+				cursor = null;
+				indexcursor = -1;
+			}
+			else if(indexcursor == 1) {
+				Node a = cursor;
+				cursor = cursor.previous;
+				indexcursor = 0;
+				if(indexcursor != length() - 1) {
+					a.previous = cursor;
+				}
+				if(indexcursor != 0) {
+					cursor.previous = null;
+				}
+			}
+		}
+		else if(cursor == null) {
+			throw new RuntimeException("Cannot move cursor when cursor is undefined");
+		}
+	}
+	void prepend(int data) {
+		if(length() > 0) {
+			Node temp = front;
+			Node N = new Node(data);
+			front = N;
+			front.next = temp;
+			front.previous = null;
+			temp.previous = front;
+		}
+		if(length() == 0) {
+			Node N = new Node(data);
+			front = N;
+			back = N;
+		}
+		if(indexcursor != -1) {
+			indexcursor++;
+		}
+		length++;
+	}
+	void append(int data) {
+		if(length() > 0) {
+			Node temp = back;
+			Node N = new Node(data);
+			back = N;
+			temp.next = N;
+			N.previous = temp;
+		}
+		if(length() == 0) {
+			Node N = new Node(data);
+			front = N;
+			back = N;
+		}
+		length++;
+	}
+	void insertBefore(int data) {
+		if(length() > 0 && index()>=0) {
+			if(cursor.previous == null) {
+				Node N = new Node(data);
+				N.next = front;
+				front = N;
+				cursor.previous = N;
+				N.previous = front;
+				length++;
+			}
+			else { 
+				Node temp = cursor.previous;
+				Node N = new Node(data);
+				temp.next = N;
+				N.next = cursor;
+				cursor.previous = N;
+				N.previous = temp;
+				length++;
+			}
+			indexcursor++;
+		}
+
+	}
+	void insertAfter(int data) {
+		if(length() > 0 && index()>=0) {
+			//if next value is null will I get an error?		
+			//Answer: yes
+			if(cursor.next != null) {
+				Node temp = cursor.next;
+				Node N = new Node(data);
+				cursor.next = N;
+				N.next = temp;
+				N.previous = cursor;
+				temp.previous = N;
+				length++;
+			}
+			else {
+				Node N = new Node(data);
+				cursor.next = N;
+				N.previous = cursor;
+				back = N;
+				length++;
+			}
+		}
+		else {
+			throw new RuntimeException("Cannot insert when a cursor is not defined");
+		}
+	}
+	void deleteFront() {
+		if(length() == 1) {
+			clear();
+		}
+		else if(length() >1) {
+			front = front.next;
+			front.previous = null;
+			length--;
+		}
+		else if (length() == 0) {
+			throw new RuntimeException("Cannot delete from an empty list");
+		}
+		if(indexcursor != -1) {
+			indexcursor--;
+		}
+	}	
+	void deleteBack() {
+		if(length() == 1) {
+			clear();
+		}
+		else if(length() >1) {
+			if(cursor == back) {
+				cursor = null;
+				indexcursor = -1;
+			}
+			back = back.previous;
+			back.next = null;
+			length--;
+		}
+		else if (length() == 0) {
+			throw new RuntimeException("Cannot delete from an empty list");    
+		}	
+	}
+	void delete() {
+		if(cursor == null) {
+			throw new RuntimeException("Cannot delete a non-existing cursor");
+		}
+		if(length() <= 0) {
+			throw new RuntimeException("Cannot delete from an empty list");
+		}
+		if(length() > 0 && indexcursor >= 0) {
+			if(cursor.previous != null && cursor.next != null) {
+				cursor.previous.next = cursor.next;
+				cursor.next.previous = cursor.previous;
+				cursor = null;
+				indexcursor = -1;
+			}
+			else if(cursor.previous == null) {
+				front = cursor.next;
+				front.previous = null;
+				cursor = null;
+				indexcursor = -1;
+			}
+			else if(cursor.next == null) {
+				back = cursor.previous;
+				back.next = null;
+				cursor = null;
+				indexcursor = -1;
+			}
+		}
+		length--;
+	}	
+
+	public String toString() {
+		String s = "";
+		if(length() != 0) {
+			Node N = front;
+			while(N != null) {
+				if(N.next != null) {
+					s = s + String.valueOf(N.value) + " ";
+				}
+				else {
+					s = s + String.valueOf(N.value);
+				}
+				N = N.next;
+
+			}	
+		}
+		return s;
+	}
+
+	List copy() {
+		List l = new List();
+		if(length() != 0) {
+			Node N = front;
+			l.append(N.value);
+			while(N.next != null) {
+				N = N.next;
+				l.append(N.value);
+			}
+		}
+		return l;
+	}
+
+}
---
+++ pa1/backup/Lex.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+import java.io.*;
+public class Lex {
+	//why throw IOException
+	public static void main (String[] args) throws IOException {
+	Scanner in = null;
+	PrintWriter out = null;
+	if(args.length != 2) {
+		System.err.println("Usage: FileIO in out");
+		System.exit(1);
+	}
+	int w = 0;
+	int l = 0;
+	in = new Scanner(new File(args[0]));
+	out = new PrintWriter(new FileWriter(args[1]));
+	
+	while(in.hasNextLine()) {
+		l++;
+		in.nextLine();	
+		}
+		in.close();
+		in = null;
+		in = new Scanner(new File(args[0]));
+		out = new PrintWriter(new FileWriter(args[1]));
+		String[] array = new String[l];
+		while(in.hasNextLine()) {
+			array[w] = in.nextLine(); 
+			w++;
+		}	
+		//in.close();
+//String[] array = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"};
+	//make a list "a"  with indexes of "array"
+//	String[] array= {"one","two","three","four","five"};//"six","seven"};
+/*	List a = new List();
+	for(int x = 0; x < array.length; x++) {
+		a.append(x);
+	}*/
+	List list = new List();
+	int i, j;
+	String temp;
+//	a.moveFront();
+	list.append(0);
+	list.moveFront();
+	for(j = 1; j < array.length; j++) {
+		temp = array[j];
+		if(j != 1)
+//		a.moveNext();
+		while(list.index() >= 0 && temp.compareTo(array[list.get()]) < 0) {
+			//a.movePrev();
+			list.movePrev();
+		}
+
+		if(list.index() < 0) {	
+			list.prepend(j);
+		}
+		else {
+			if(list.index() != list.length()-1) {
+				list.insertAfter(j);
+			}
+			else {
+				list.append(j);
+			}
+		}
+		 list.moveBack();
+//	if(a.index() < 0) {
+//		a.moveFront();
+//	}
+//		while(a.index() != j-1) {
+//			a.moveNext();
+//		}
+	}
+	list.moveFront();
+	while(list.index() > -1) {
+		out.println(array[list.get()]);
+		list.moveNext();
+	}
+	in.close();
+	out.close();
+}
+}
